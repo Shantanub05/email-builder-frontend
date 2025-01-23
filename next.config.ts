874d200b096afff98071f39bc8bc84ce285b30ab
Shantanu-Bokey <shantanu.bokey@roxiler.com>
2025-01-23 12:41:40 +0530

    updated configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // 👈 Add this here
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
